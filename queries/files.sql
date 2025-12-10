126afa92d46e5e4c5f15ed4ca657d7a0f582078a
amirhnajafiz <najafizadeh21@gmail.com>
2025-12-10 17:18:06 -0500

    add: raw queries

--- queries/files.sql
+++ queries/files.sql
@@ -5,3 +5,35 @@ WHERE fname is NOT NULL AND fname <> ''
 GROUP BY fname, event_name
 HAVING event_name LIKE '%read%' OR event_name LIKE '%write%'
 ORDER BY fcount;
+
+--- Find the percentage of unknown operations
+SELECT 
+    (100.0 * 
+        SUM(CASE WHEN fname IS NULL OR fname = '' OR fname = 'unknown' THEN 1 ELSE 0 END)
+    ) / NULLIF(COUNT(*), 0) 
+AS percent_unknown
+FROM io_logs;
+
+--- Find the percentage of read operations
+SELECT 
+    (100.0 * 
+        SUM(CASE WHEN event_name LIKE '%read%' THEN 1 ELSE 0 END)
+    ) / NULLIF(COUNT(*), 0) 
+AS percent_read
+FROM io_logs;
+
+--- Find the percentage of write operations
+SELECT 
+    (100.0 * 
+        SUM(CASE WHEN event_name LIKE '%write%' THEN 1 ELSE 0 END)
+    ) / NULLIF(COUNT(*), 0) 
+AS percent_write
+FROM io_logs;
+
+--- Find the percentage of mmap operations
+SELECT 
+    (100.0 * 
+        SUM(CASE WHEN event_name LIKE '%mmap%' THEN 1 ELSE 0 END)
+    ) / NULLIF(COUNT(*), 0) 
+AS percent_mmap
+FROM io_logs;
