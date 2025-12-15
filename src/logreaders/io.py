ea085a28d518198f6bea5e9e15ac14bf559d0001
amirhnajafiz <najafizadeh21@gmail.com>
2025-12-15 12:38:47 -0500

    update: log reader format

--- src/logreaders/io.py
+++ src/logreaders/io.py
@@ -1,4 +1,5 @@
-import os
+import logging
+from pathlib import Path
 
 from src.database.models import IOLog
 from src.logreaders import Reader
@@ -10,59 +11,72 @@ class IOReader(Reader):
     def name(self) -> str:
         return "io"
 
+    def get_log_files(self) -> list[str]:
+        root = Path(self.dir_path)
+        return [x for x in root.rglob("trace_io_*.log")]
+
     def start(self) -> bool:
         hashmap = {}  # map to merge log events
         batch = []  # a list to store IO logs in batch
 
         # reader params
-        filepath = os.path.join(self.dir_path, "io_logs.txt")
-        limit = self.batch_size
+        files = self.get_log_files()
 
-        # read the logs line by line
-        with open(filepath, "r") as file:
-            for line in file:
-                # read the line
-                m = self.match_string(line)
-                if not m:
-                    continue
+        logging.debug(f"reader {self.name()}: files={len(files)}")
 
-                # convert it into an object
-                obj = self.parse_match_into_dictionary(m)
+        for filepath in files:
+            logging.debug(f"reader {self.name()} is reading {filepath}")
 
-                # form the key
-                key = (obj["pid"], obj["tid"])
+            limit = self.batch_size
 
-                if obj["status"] == "EN":
-                    hashmap[key] = obj
-                elif key in hashmap:
-                    en_obj = hashmap[key]
-                    del hashmap[key]
+            # read the logs line by line
+            with open(filepath, "r") as file:
+                for line in file:
+                    # read the line
+                    m = self.match_string(line)
+                    if not m:
+                        continue
 
-                    fd = int(en_obj["spec"].get("fd", -1))
-                    ret = int(obj["spec"].get("ret", -1))
+                    # convert it into an object
+                    obj = self.parse_match_into_dictionary(m)
 
-                    # exclude the negative records
-                    if fd > -1 and ret > -1:
-                        batch.append(
-                            IOLog(
-                                en_timestamp=int(en_obj["timestamp"]),
-                                en_datetime=en_obj["datetime"].isoformat(" "),
-                                ex_timestamp=int(obj["timestamp"]),
-                                ex_datetime=obj["datetime"].isoformat(" "),
-                                latency=(obj["datetime"] - en_obj["datetime"]).total_seconds() * (10**9),
-                                pid=obj["pid"],
-                                tid=obj["tid"],
-                                proc=obj["proc"],
-                                event_name=obj["operand"],
-                                fd=fd,
-                                ret=ret,
-                                countbytes=int(en_obj["spec"].get("count", 0)),
+                    # form the key
+                    key = (obj["pid"], obj["tid"])
+
+                    if obj["status"] == "EN":
+                        hashmap[key] = obj
+                    elif key in hashmap:
+                        en_obj = hashmap[key]
+                        del hashmap[key]
+
+                        fd = int(en_obj["spec"].get("fd", -1))
+                        ret = int(obj["spec"].get("ret", -1))
+
+                        # exclude the negative records
+                        if fd > -1 and ret > -1:
+                            batch.append(
+                                IOLog(
+                                    en_timestamp=int(en_obj["timestamp"]),
+                                    en_datetime=en_obj["datetime"].isoformat(" "),
+                                    ex_timestamp=int(obj["timestamp"]),
+                                    ex_datetime=obj["datetime"].isoformat(" "),
+                                    latency=(
+                                        obj["datetime"] - en_obj["datetime"]
+                                    ).total_seconds()
+                                    * (10**9),
+                                    pid=obj["pid"],
+                                    tid=obj["tid"],
+                                    proc=obj["proc"],
+                                    event_name=obj["operand"],
+                                    fd=fd,
+                                    ret=ret,
+                                    countbytes=int(en_obj["spec"].get("count", 0)),
+                                )
                             )
-                        )
 
-                if len(batch) > limit:
-                    self.db.batch_insert(batch)
-                    batch = []
+                    if len(batch) > limit:
+                        self.db.batch_insert(batch)
+                        batch = []
 
-        if len(batch) > 0:
-            self.db.batch_insert(batch)
+            if len(batch) > 0:
+                self.db.batch_insert(batch)
--- src/logreaders/meta.py
+++ src/logreaders/meta.py
@@ -1,4 +1,5 @@
-import os
+import logging
+from pathlib import Path
 
 from src.database.models import MetaLog
 from src.logreaders import Reader
@@ -10,57 +11,70 @@ class MetaReader(Reader):
     def name(self) -> str:
         return "meta"
 
+    def get_log_files(self) -> list[str]:
+        root = Path(self.dir_path)
+        return [x for x in root.rglob("trace_meta_*.log")]
+
     def start(self) -> bool:
         hashmap = {}  # map to merge log events
         batch = []  # a list to store logs in batch
 
         # reader params
-        filepath = os.path.join(self.dir_path, "meta_logs.txt")
-        limit = self.batch_size
+        files = self.get_log_files()
 
-        # read the logs line by line
-        with open(filepath, "r") as file:
-            for line in file:
-                # read the line
-                m = self.match_string(line)
-                if not m:
-                    continue
+        logging.debug(f"reader {self.name()}: files={len(files)}")
 
-                # convert it into an object
-                obj = self.parse_match_into_dictionary(m)
+        for filepath in files:
+            logging.debug(f"reader {self.name()} is reading {filepath}")
 
-                # form the key
-                key = (obj["pid"], obj["tid"])
+            limit = self.batch_size
 
-                if obj["status"] == "EN":
-                    hashmap[key] = obj
-                elif key in hashmap:
-                    en_obj = hashmap[key]
-                    del hashmap[key]
+            # read the logs line by line
+            with open(filepath, "r") as file:
+                for line in file:
+                    # read the line
+                    m = self.match_string(line)
+                    if not m:
+                        continue
 
-                    ret = int(obj["spec"].get("ret", -1))
+                    # convert it into an object
+                    obj = self.parse_match_into_dictionary(m)
 
-                    # exclude the negative records
-                    if ret > -1:
-                        batch.append(
-                            MetaLog(
-                                en_timestamp=int(en_obj["timestamp"]),
-                                en_datetime=en_obj["datetime"].isoformat(" "),
-                                ex_timestamp=int(obj["timestamp"]),
-                                ex_datetime=obj["datetime"].isoformat(" "),
-                                latency=(obj["datetime"] - en_obj["datetime"]).total_seconds() * (10**9),
-                                pid=obj["pid"],
-                                tid=obj["tid"],
-                                proc=obj["proc"],
-                                event_name=obj["operand"],
-                                fname=en_obj["spec"].get("fname", "unknown"),
-                                ret=ret,
+                    # form the key
+                    key = (obj["pid"], obj["tid"])
+
+                    if obj["status"] == "EN":
+                        hashmap[key] = obj
+                    elif key in hashmap:
+                        en_obj = hashmap[key]
+                        del hashmap[key]
+
+                        ret = int(obj["spec"].get("ret", -1))
+
+                        # exclude the negative records
+                        if ret > -1:
+                            batch.append(
+                                MetaLog(
+                                    en_timestamp=int(en_obj["timestamp"]),
+                                    en_datetime=en_obj["datetime"].isoformat(" "),
+                                    ex_timestamp=int(obj["timestamp"]),
+                                    ex_datetime=obj["datetime"].isoformat(" "),
+                                    latency=(
+                                        obj["datetime"] - en_obj["datetime"]
+                                    ).total_seconds()
+                                    * (10**9),
+                                    pid=obj["pid"],
+                                    tid=obj["tid"],
+                                    proc=obj["proc"],
+                                    event_name=obj["operand"],
+                                    fname=en_obj["spec"].get("fname", "unknown"),
+                                    ret=ret,
+                                )
                             )
-                        )
 
-                if len(batch) > limit:
-                    self.db.batch_insert(batch)
-                    batch = []
+                    if len(batch) > limit:
+                        self.db.batch_insert(batch)
+                        batch = []
 
-        if len(batch) > 0:
-            self.db.batch_insert(batch)
+            if len(batch) > 0:
+                self.db.batch_insert(batch)
