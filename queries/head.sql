34402995e3f10b7c753cdd0ec41de54ffbdb5f97
amirhnajafiz <najafizadeh21@gmail.com>
2026-01-12 12:24:32 -0500

    remove: python code

--- queries/head.sql
+++
@@ -1,2 +0,0 @@
--- Sample io_logs for analysis
-SELECT * FROM io_logs LIMIT 10;
--- src/api/views.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-
-
-def index():
-    """
-    Render the index.html file.
-    :return: The rendered index.html file.
-    """
-    return render_template("index.html")
