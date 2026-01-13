af983edfc653a910ffab784a4d9b81a89dd9fa48
amirhnajafiz <najafizadeh21@gmail.com>
2026-01-13 15:08:15 -0500

    refactor: reader

--- include/workers/reader.go
+++ include/workers/reader.go
@@ -12,8 +12,6 @@ import (
 
 	"github.com/amirhnajafiz/flak-dashboard/pkg/hashing"
 	"github.com/amirhnajafiz/flak-dashboard/pkg/models"
-
-	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -21,61 +19,33 @@ var (
 	re = regexp.MustCompile(`^(\d+)\s+\{pid=(\d+)\s+tid=(\d+)\s+proc=([^\}]+)\}\{(EN|EX)\s+([^\}]+)\}(?:\{([^\}]*)\})?$`)
 )
 
-// reader gets a file, offset, and chunkSize to read a partition of a file and pass
-// it's lines to the reader-reductor distributor.
+// reader gets a file, an offset, and a chunk size to read a partition of the given file.
 type reader struct {
-	// unique id of the partition of the file
-	id int
+	id                       int
+	numberOfReductors        int
+	readerReductorInFlightWg *sync.WaitGroup
+	reductorChannels         map[int]chan models.Packet
 
 	// read parameters
 	offset    int64
 	chunkSize int64
 	fileSize  int64
-
-	// target file
-	filePath string
-
-	// reductor channels
-	reductorChannels  map[int]chan models.Packet
-	numberOfReductors int
-
-	// wg
-	readerReductorInFlightWg *sync.WaitGroup
+	filePath  string
 }
 
 // start the reader worker.
-func (r *reader) start() {
+func (r *reader) start() error {
 	// open the log file
 	fd, err := os.Open(r.filePath)
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"id":    r.id,
-			"error": err,
-			"path":  r.filePath,
-		}).Error("failed to open the target file")
-
-		return
+		return fmt.Errorf("failed to open file: %v", err)
 	}
 	defer fd.Close()
-
-	logrus.WithFields(
-		logrus.Fields{
-			"id":    r.id,
-			"start": r.offset,
-			"end":   r.offset + r.chunkSize,
-		},
-	).Info("reader start")
 
 	// seek to chunk start
 	_, err = fd.Seek(r.offset, io.SeekStart)
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"id":    r.id,
-			"error": err,
-			"path":  r.filePath,
-		}).Error("seek to chunk start failed")
-
-		return
+		return fmt.Errorf("failed to seek: %v", err)
 	}
 
 	// create a reader of 64Kb size
@@ -93,6 +63,7 @@ func (r *reader) start() {
 		}
 	}
 
+	// read the partition line by line
 	for {
 		line, err := reader.ReadString('\n')
 		if len(line) > 0 {
@@ -111,17 +82,23 @@ func (r *reader) start() {
 			break
 		}
 	}
+
+	return nil
 }
 
-// In log handler, match with the regex and skip if not matched.
-// Else, extract and build the transfer packet and send it to the distributor.
+// In log handler, a line is matched with the regex and skipped if not matching.
+// Otherwise, handler builds a packet and sends it to the reductor.
 func (r *reader) logHandler(line string) {
+	// trim the line
 	line = strings.TrimSpace(line)
+
+	// match with the regex
 	match := re.FindStringSubmatch(line)
 	if match == nil {
 		return
 	}
 
+	// extract metadata
 	timestamp, _ := strconv.ParseInt(match[1], 10, 64)
 	pid, _ := strconv.Atoi(match[2])
 	tid, _ := strconv.Atoi(match[3])
@@ -136,27 +113,24 @@ func (r *reader) logHandler(line string) {
 		}
 	}
 
-	// create a new event
-	event := models.TraceEvent{
-		Timestamp: timestamp,
-		PID:       pid,
-		TID:       tid,
-		Proc:      match[4],
-		EventType: match[5],
-		Event:     match[6],
-		KV:        kv,
-	}
-
 	// create a packet
 	pkt := models.Packet{
 		PartitionIndex: r.id,
 		PartitionName:  r.filePath,
-		TraceKey:       fmt.Sprintf("%d-%d-%s-%s", event.PID, event.TID, event.Proc, event.Event),
-		TraceEvent:     event,
+		TraceKey:       fmt.Sprintf("%d-%d-%s-%s", pid, tid, match[4], match[6]),
 		TraceEventRaw:  line,
+		TraceEvent: models.TraceEvent{
+			Timestamp: timestamp,
+			PID:       pid,
+			TID:       tid,
+			Proc:      match[4],
+			EventType: match[5],
+			Event:     match[6],
+			KV:        kv,
+		},
 	}
 
-	// find the reductor
+	// find the reductor using a hash range function
 	index := hashing.HashToRange(pkt.TraceKey, uint64(r.numberOfReductors))
 
 	// submit the event
