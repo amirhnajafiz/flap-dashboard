a6145a11176f3f6a163575f54ec8dcf6869ca400
amirhnajafiz <najafizadeh21@gmail.com>
2026-01-14 16:02:55 -0500

    feat: interpreter handler logic

--- include/components/interpreter/handlers.go
+++ include/components/interpreter/handlers.go
@@ -9,23 +9,19 @@ type handlerFunc func(string, string, string, map[string]string)
 
 func (i *Interpreter) handleIOSyscall(ts, proc, operand string, kv map[string]string) {
 	ret, _ := strconv.Atoi(kv["ret"])
-	fd, _ := strconv.Atoi(kv["fd"])
-
 	if ret < 0 {
 		return
 	}
+
+	fd, _ := strconv.Atoi(kv["fd"])
 
 	fname := i.fdt.search(proc, fd)
 	if len(fname) == 0 {
 		fname = fmt.Sprintf("unknown (fd=%d)", fd)
 	}
 
-	fmt.Printf("%s [%s : %s] on: %s\n", ts, proc, operand, fname)
+	fmt.Printf("%s+%s [%s : %s] %s bytes file: %s\n", ts, kv["diff"], proc, operand, fname, kv["count"])
 }
-
-func (i *Interpreter) handleMemorySyscall(ts, proc, operand string, kv map[string]string) {}
-
-func (i *Interpreter) handleAddressSpaceSyscall(ts, proc, operand string, kv map[string]string) {}
 
 func (i *Interpreter) handleFdTableSyscall(ts, proc, operand string, kv map[string]string) {
 	ret, _ := strconv.Atoi(kv["ret"])
@@ -41,4 +37,51 @@ func (i *Interpreter) handleFdTableSyscall(ts, proc, operand string, kv map[stri
 	}
 }
 
-func (i *Interpreter) handleUpdateFdSyscall(ts, proc, operand string, kv map[string]string) {}
+func (i *Interpreter) handleUpdateFdSyscall(ts, proc, operand string, kv map[string]string) {
+	ret, _ := strconv.Atoi(kv["ret"])
+	if ret < 0 {
+		return
+	}
+
+	newFd, _ := strconv.Atoi(kv["newfd"])
+	oldFd, _ := strconv.Atoi(kv["oldfd"])
+
+	i.fdt.replace(proc, oldFd, newFd)
+}
+
+func (i *Interpreter) handleMemorySyscall(ts, proc, operand string, kv map[string]string) {
+	ret, _ := strconv.Atoi(kv["ret"])
+	if ret < 0 {
+		return
+	}
+
+	address, _ := strconv.ParseInt(kv["addr"], 10, 64)
+	fd := i.vma.search(proc, address)
+
+	fname := i.fdt.search(proc, fd)
+	if len(fname) == 0 {
+		fname = fmt.Sprintf("unknown (fd=%d)", fd)
+	}
+
+	fmt.Printf("%s+%s [%s : %s] memory_access: %s\n", ts, kv["diff"], proc, operand, fname)
+}
+
+func (i *Interpreter) handleAddressSpaceSyscall(ts, proc, operand string, kv map[string]string) {
+	ret, _ := strconv.Atoi(kv["ret"])
+	if ret < 0 {
+		return
+	}
+
+	address, _ := strconv.ParseInt(kv["addr"], 10, 64)
+	boundary, _ := strconv.ParseInt(kv["len"], 10, 64)
+
+	if operand == "mmap" {
+		fd, _ := strconv.Atoi(kv["fd"])
+		i.vma.put(proc, fd, address, boundary)
+	} else {
+		fd := i.vma.search(proc, address)
+		if fd > -1 {
+			i.vma.remove(proc, fd)
+		}
+	}
+}
